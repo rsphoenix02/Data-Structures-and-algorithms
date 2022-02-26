379268f29859f13a7350400e205a6f0530c43e87
samdev-py <jimisierra2@gmail.com>
2022-02-26 15:25:46 +0530

    DSA

--- Linked List/LinkedList.cpp
+++ Linked List/LinkedList.cpp
@@ -1,156 +1,53 @@
 #include <iostream>
-#include "Node.cpp"
 using namespace std;
-Node *takeInput()
+class Node
 {
+public:
     int data;
-    cin >> data;
-    Node *head = NULL;
-    Node *tail = NULL;
-    while (data != -1)
-    {
-        Node *newNode = new Node(data);
-        if (head == NULL)
-        {
-            head = newNode;
-            tail = newNode;
-        }
-        else
-        {
-            tail->next = newNode;
-            tail = tail->next;
-        }
-        cin >> data;
-    }
-    return head;
-}
+    Node *next;
 
-// Count Nodes using recursion
-int size(Node *p)
-{
-    if (p == NULL)
+    Node()
     {
-        return 0;
+        data = 0;
+        next = NULL;
     }
-    else
+    Node(int val)
     {
-        return 1 + size(p->next);
+        data = val;
+        next = NULL;
     }
-}
-
-//  Sum using recursion
-int sum(Node *p)
+};
+// Insert node at last location
+void insertAtLast(Node *&head, int val)
 {
 
-    if (p == 0)
-    {
-        return 0;
-    }
-    else
-    {
-        return sum(p->next) + p->data;
-    }
-}
-
-// Finding Maximum Element in LinkedList
-int maxElement(Node *p)
-{
-    int max = 0;
-    while (p != NULL)
-    {
-        if ((p->data) > max)
-        {
-            max = p->data;
-        }
-        p = p->next;
-    }
-    return max;
-}
-// Linear Search to find element in linear search
-int search(Node *p, int x)
-{
-    int count = 0;
-    while (p != NULL)
-    {
-        if (p->data == x)
-        {
-            return count;
-        }
-        else
-        {
-            p = p->next;
-            count++;
-        }
-    }
-    return -1;
-}
-void insert(Node *p, int element, int index)
-{
-    if ((index < 0) or (index > size(p)))
-    {
-        return;
-    }
-
-    Node *t = new Node(element);
-    int count = 0;
-    Node *temp = p;
-
-    while (count < index - 1)
-    {
-        temp = temp->next;
-        count++;
-    }
-    t->next = temp->next;
-    temp->next = t;
-}
-void insertLast(Node *head, int x)
-{
-    Node *t = new Node(x);
+    Node *p = new Node(val);
     Node *temp = head;
-
-    while (temp->next != NULL)
+    if (head == NULL)
     {
-        temp = temp->next;
-    }
-    temp->next = t;
-    t->next = NULL;
-}
-
-bool isSorted(Node *p)
-{
-    bool Sorted = true;
-    while (p->next != NULL)
-    {
-        if ((p->next->data) < (p->data))
-        {
-            Sorted = false;
-        }
-        p = p->next;
-    }
-    return Sorted;
-}
-void sortedInsert(Node *head_ref,
-                  int num)
-{
-    Node *temp = new Node(num);
-    if (head_ref->data >= num)
-    {
-        temp->next = head_ref;
-        head_ref = temp;
+        head = p;
+        p->next = NULL;
     }
     else
     {
-        Node *top = head_ref;
-        while (top != NULL && top->data < num)
+        while (temp->next != NULL)
         {
-            top = top->next;
+            temp = temp->next;
         }
-        top->next = temp;
-        temp->next = top->next->next;
+        temp->next = p;
+        p->next = NULL;
     }
 }
-
-/* Function to print linked list */
+// Insert node at first position
+void insertAtHead(Node *&head, int val)
+{
+    Node *p = new Node(val);
+    Node *temp = new Node;
+    temp = head;
+    p->next = temp;
+    head = p;
+}
+// Print all nodes
 void display(Node *head)
 {
     Node *temp = head;
@@ -159,30 +56,128 @@ void display(Node *head)
         cout << temp->data << " ";
         temp = temp->next;
     }
-}
-int main()
-{
-
-    Node *Head = takeInput();
-    display(Head);
     cout << endl;
-    if (isSorted(Head) == true)
+}
+// Insert in sorted linked List
+void insertSorted(Node *&head, int val)
+{
+    Node *p = new Node(val);
+    Node *temp = head;
+    if (head == NULL)
     {
-        cout << " It is sorted";
+        head = p;
+    }
+    else if (head->data >= val)
+    {
+        p->next = head;
+        head = p;
     }
     else
     {
-        cout << "It is unsorted";
+        while (temp->next->data < val)
+        {
+            temp = temp->next;
+        }
+        p->next = temp->next;
+        temp->next = p;
     }
-    cout << endl;
-    cout << Head << "\n";
-    int n;
-    cout << "Enter new value"
-         << "\n";
-    cin >> n;
-    sortedInsert(Head, n);
-    cout << Head << endl;
-    display(Head);
+}
+// If value is present in linked List
+bool search(Node *head, int val)
+{
+    Node *temp = head;
+    bool present = false;
+    while (temp != NULL)
+    {
+        if (temp->data == val)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+// Delete a value from linkedlist
+void deletion(Node *&head, int val)
+{
+    Node *temp = head;
+    if (head->data == val)
+    {
+        head = head->next;
+        delete temp;
+    }
+    else
+    {
+        while (temp->next->data != val)
+        {
+            temp = temp->next;
+        }
+        Node *toDelete = temp->next;
+        temp->next = temp->next->next;
+        delete toDelete;
+    }
+}
+// Returns if sorted or not
+bool isSorted(Node *head)
+{
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        if (temp->data > temp->next->data)
+        {
+            return false;
+        }
 
+        temp = temp->next;
+    }
+    return true;
+}
+// Remove continously coming dublicate elements from linkedlist
+void removeDublicates(Node *&head)
+{
+    Node *p, *q;
+    p = head;
+    q = p->next;
+    while (q != NULL)
+    {
+        if (p->data != q->data)
+        {
+            p = q;
+            q = q->next;
+        }
+        else
+        {
+            p->next = q->next;
+            delete q;
+            q = p->next;
+        }
+    }
+}
+int main()
+
+{
+    Node *head = NULL;
+    insertAtLast(head, 4);
+    insertAtLast(head, 8);
+    insertAtLast(head, 10);
+    insertAtHead(head, 2);
+    insertAtHead(head, 2);
+    insertSorted(head, 1);
+    display(head);
+    cout << search(head, 4) << endl;
+    deletion(head, 4);
+    display(head);
+    isSorted(head);
+
+    if (isSorted(head) == 0)
+    {
+        cout << "It is unsorted" << endl;
+    }
+    else
+    {
+        cout << "Sorted" << endl;
+    }
+    removeDublicates(head);
+    display(head);
     return 0;
 }
