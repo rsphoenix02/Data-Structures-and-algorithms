b8cc6d044d2d38ef0d25447984afa20c49f705bf
samdev-py <jimisierra2@gmail.com>
2022-03-15 18:07:12 +0530

    Data Structures and Algorithms

--- 03. Arrays/Array Problems/Merge2SortedArrays.cpp
+++ 03. Arrays/Array Problems/Merge2SortedArrays.cpp
@@ -1,40 +1,46 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void merge(int arr1[], int n, int arr2[], int m, int arr3[])
+vector<int> merge(int nums1[], int n, int nums2[], int m)
 {
     int i = 0, j = 0;
-    int k = 0;
+    vector<int> v;
     while (i < n and j < m)
     {
-        if (arr1[i] < arr2[j])
+        if (nums1[i] <= nums2[j])
         {
-            arr3[k++] = arr1[i++];
+            v.push_back(nums1[i]);
+            i++;
         }
         else
         {
-            arr3[k++] = arr2[j++];
+            v.push_back(nums2[j]);
+            j++;
         }
     }
     while (i < n)
     {
-        arr3[k++] = arr1[i++];
+        v.push_back(nums1[i]);
+        i++;
     }
     while (j < m)
     {
-        arr3[k++] = arr2[j++];
+        v.push_back(nums2[j]);
+        j++;
     }
+
+    return v;
 }
 
 int main()
 {
     int arr1[5] = {1, 3, 5, 7, 9};
     int arr2[3] = {2, 4, 6};
-    int arr3[8] = {0};
 
-    merge(arr1, 5, arr2, 3, arr3);
+    vector<int> arr3 = merge(arr1, 5, arr2, 3);
 
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < arr3.size(); i++)
     {
         cout << arr3[i] << " ";
     }
